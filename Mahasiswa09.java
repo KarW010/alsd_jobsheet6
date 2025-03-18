3f192973e62ff003bb69115ac42d01205ba9c55c
KarW010 <kartikostudy@gmail.com>
2025-03-18 14:17:13 +0700

    5.3 Praktikum 2

---
+++ Mahasiswa09.java
@@ -0,0 +1,23 @@
+public class Mahasiswa09{
+    String nim; 
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa09(){
+    }
+
+    Mahasiswa09(String nm, String name, String kls, double ip){
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
